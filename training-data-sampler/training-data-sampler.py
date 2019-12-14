878cf50a439d29be1fbc2577be9c3220d4d72772
Victor Freire <victorclf@live.com>
2019-12-14 10:56:21 -0300

    Removed old commented out code from training data sampler; Update training data with new sample.

--- training-data-sampler/training-data-sampler.py
+++ training-data-sampler/training-data-sampler.py
@@ -22,31 +22,7 @@ DATASET_PATH = '../preprocess-dataset/output/reviews.csv'
 TRAINING_DATASET_SIZE = 1000
 OUTPUT_PATH = 'output/discussions-sample-%d.csv' % TRAINING_DATASET_SIZE
 
-# ~ def mergeCSV(csvFilePaths, outputPath):
-	# ~ for csvPath in csvFilePaths:
-		# ~ if os.path.exists(csvPath):
-			# ~ with open(csvPath, 'r') as fin:
-				# ~ headerLine = fin.readline()
-				
-				# ~ if not os.path.exists(outputPath):
-					# ~ with open(outputPath, 'w') as fout:
-						# ~ fout.write(headerLine)
-				
-				# ~ with open(outputPath, 'a') as fout:
-					# ~ for line in fin:
-						# ~ fout.write(line)
-						
 
-# ~ def getRandomSampleOfReviews():
-	# ~ csvFiles = []
-	# ~ for root, dirs, files in os.walk(DATASET_FOLDER):
-		# ~ for f in files:
-			# ~ if f.startswith('review') and f.endswith('csv'):
-				# ~ csvFiles.append(os.path.join(root, f))
-	# ~ selectedCsvFiles = random.choices(csvFiles, k = TRAINING_DATASET_SIZE)
-	# ~ return selectedCsvFiles
-
-	
 def getRandomSampleOfDiscussions(sampleSize):
 	discussions = []
 	with open(DATASET_PATH, 'r') as fin:
@@ -70,8 +46,5 @@ def writeDiscussionsToCSV(discussions, outputPath):
 
 
 if __name__ == '__main__':
-	# selectedCsvFiles = [os.path.join(DATASET_FOLDER, 'review-%d.csv' % rid) for rid in range(69000, 69100)]
-	#selectedCsvFiles = getRandomSampleOfReviews()
-	#mergeCSV(selectedCsvFiles, OUTPUT_PATH)
 	sampleDiscussions = getRandomSampleOfDiscussions(TRAINING_DATASET_SIZE)
 	writeDiscussionsToCSV(sampleDiscussions, OUTPUT_PATH)
