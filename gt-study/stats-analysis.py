f24035ad9b9b5736d864dfed4ae369c58f69869d
Victor Freire <victorclf@live.com>
2021-03-02 17:50:40 -0300

    Add repository and developer stats to analysis statistics script.

--- gt-study/stats-analysis.py
+++ gt-study/stats-analysis.py
@@ -3,6 +3,7 @@ import os
 import sys
 import subprocess
 import csv
+import re
 
 def getBasicAnalysisStats(basePath):
 	totalReviewRequests = 0
@@ -38,6 +39,32 @@ def getConceptStats(markedPassagesCsvPath):
 	return len(concepts), annotations
 	
 
+def getReviewRequestStats(basePath):
+	repoNameRegex = re.compile('repository (.*?) at ')
+	developerRegex = re.compile('by (.*?) ')
+	
+	repoFrequency = {}
+	developerFrequency = {}
+	
+	for root, dir, files in os.walk(basePath):
+		for f in files:
+			if f.startswith('review') and f.endswith('txt'):
+				filePath = os.path.join(root, f)
+				with open(filePath, encoding='utf-16') as fin:
+					for line in fin.readlines():
+						if line.strip().startswith('$$$Review Request'):
+							repo = repoNameRegex.search(line).group(1)
+							repoFrequency[repo] = repoFrequency.get(repo, 0) + 1
+							
+						if line.strip().startswith('$$$'):
+							regexResult = developerRegex.search(line)
+							if regexResult:
+								developer = regexResult.group(1)
+								developerFrequency[developer] = developerFrequency.get(developer, 0) + 1
+
+	return repoFrequency, developerFrequency
+	
+
 if __name__ == '__main__':
 	reviewRequests, comments, words = getBasicAnalysisStats('output/qcamap')
 	print(f"Review requests: {reviewRequests}")
@@ -48,5 +75,10 @@ if __name__ == '__main__':
 	print(f"Concepts: {concepts}")
 	print(f"Annotations: {annotations}")
 	
-		
-			
+	repoFrequency, developerFrequency = getReviewRequestStats('output/qcamap')
+	print(f"Repositories ({len(repoFrequency)}) and review requests:")
+	for repo in sorted(repoFrequency):
+		print(f"\t{repo}: {repoFrequency[repo]}")
+	print(f"Developers ({len(developerFrequency)}) and comments:")
+	for developer in sorted(developerFrequency):
+		print(f"\t{developer}: {developerFrequency[developer]}")
